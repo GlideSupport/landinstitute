a50d8db028ebba7bdb090a4133d971b5f167540d
GLIDE Design - Kush <aakash@glidedesign.com>
2025-06-19 11:57:58 +0530

    Container plugin image added

--- wp-content/themes/landinstitute/blocks/section-container/index.asset.php
+++ wp-content/themes/landinstitute/blocks/section-container/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'd719da0eaa15a8771726');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'c6712f32b8f2285da679');
