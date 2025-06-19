b17a84c3c7eca9b33aa5e115167f35714d03b61f
Glide Design - Badal <aakash@glidedesign.com>
2025-06-19 12:24:15 +0530

    Design Backend code changes

--- wp-content/themes/landinstitute/blocks/section-container/index.asset.php
+++ wp-content/themes/landinstitute/blocks/section-container/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'e8c8bfe2cd40bb05cbc0');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '7c839ee1e8af7df5c3ee');
