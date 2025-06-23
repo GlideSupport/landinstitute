00d0f7f6afcc9452cce9ef8bf15e25fb3645ba75
Glide - Rohan <support@glidedesign.com>
2025-06-23 20:08:28 +0530

    dev changes

--- wp-content/themes/landinstitute/partials/content-staff.php
+++ wp-content/themes/landinstitute/partials/content-staff.php
@@ -27,65 +27,69 @@ $staff_email = get_field('staff_email_address', $bst_var_post_id);
 		<div class="staff-detail-layout bg-lilac">
 			<div class="row-flex">
 				<div class="col-left">
-					<div class="sticky-block">
-						<div class="back-btn">
-							<a class="site-btn arrow-pointing-left btn-lilac" href="/staff-directory/" title="" role="Button"
-								aria-label="Button">Back to All Staff
-							</a>
-						</div>
-						<div class="staff-image">
-							<?php
-							if (! has_post_thumbnail($bst_var_post_id)) {
-								echo '<img class="" src="' . esc_url(get_template_directory_uri()) . '/assets/build/images/admin/defaults/default-avatar.webp" >';
-							} else {
-								echo get_the_post_thumbnail($bst_var_post_id, 'thumb_900',);
-							}
-							?>
+					<div class="sticky-parent">
+							<div class="sticky-top-touch">
+								<div class="back-btn">
+									<a class="site-btn arrow-pointing-left btn-lilac" href="/staff-directory/" title="" role="Button"
+										aria-label="Button">Back to All Staff
+									</a>
+								</div>
+								<div class="staff-image">
+									<?php
+									if (! has_post_thumbnail($bst_var_post_id)) {
+										echo '<img class="" src="' . esc_url(get_template_directory_uri()) . '/assets/build/images/admin/defaults/default-avatar.webp" >';
+									} else {
+										echo get_the_post_thumbnail($bst_var_post_id, 'thumb_900',);
+									}
+									?>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="col-right">
-					<div class="gl-s80"></div>
-					<h1 class="block-title heading-1 mb-0">
-						<?php echo esc_html($bst_var_staff_title); ?>
-					</h1>
-					<div class="gl-s12"></div>
-					<?php if ($staff_designation): ?>
-						<div class="ui-eyebrow-18-16-regular sub-head">
-							<?php echo html_entity_decode($staff_designation); ?>
-						</div>
-					<?php endif; ?>
-					<?php //if ($staff_designation || $staff_bio_description): ?>
+					<div class="col-right">
+						<div class="gl-s80"></div>
+						<h1 class="block-title heading-1 mb-0">
+							<?php echo esc_html($bst_var_staff_title); ?>
+						</h1>
+						<div class="gl-s12"></div>
+						<?php if ($staff_designation): ?>
+							<div class="ui-eyebrow-18-16-regular sub-head">
+								<?php echo html_entity_decode($staff_designation); ?>
+							</div>
+						<?php endif; ?>
+						<?php //if ($staff_designation || $staff_bio_description): 
+						?>
 
-					<?php //endif; ?>
-					<?php if ($staff_bio_description): ?>
+						<?php //endif; 
+						?>
+						<?php if ($staff_bio_description): ?>
+							<div class="gl-s52"></div>
+							<h3 class="block-inner-heading heading-3 mb-0">Bio</h3>
+							<div class="gl-s16"></div>
+							<div class="block-content body-20-18-regular">
+								<?php echo $staff_bio_description; ?>
+							</div>
+						<?php endif; ?>
 						<div class="gl-s52"></div>
-						<h3 class="block-inner-heading heading-3 mb-0">Bio</h3>
-						<div class="gl-s16"></div>
-						<div class="block-content body-20-18-regular">
-							<?php echo $staff_bio_description; ?>
-						</div>
-					<?php endif; ?>
-					<div class="gl-s52"></div>
-					<?php if ($staff_email): ?>
-						<h3 class="block-inner-heading heading-3 mb-0">Contact Info</h3>
-						<div class="gl-s16"></div>
-						<div class="user-info body-20-18-regular">
-							<span>Email:</span>
-							<a
-								href="mailto:<?php echo html_entity_decode($staff_email); ?>">
-								<?php echo html_entity_decode($staff_email); ?>
-							</a>
-						</div>
-						<div class="gl-s52"></div>
-					<?php endif; ?>
+						<?php if ($staff_email): ?>
+							<h3 class="block-inner-heading heading-3 mb-0">Contact Info</h3>
+							<div class="gl-s16"></div>
+							<div class="user-info body-20-18-regular">
+								<span>Email:</span>
+								<a
+									href="mailto:<?php echo html_entity_decode($staff_email); ?>">
+									<?php echo html_entity_decode($staff_email); ?>
+								</a>
+							</div>
+							<div class="gl-s52"></div>
+						<?php endif; ?>
 
-					<?php get_template_part('partials/content'); ?>
+						<?php get_template_part('partials/content'); ?>
 
 
-					<div class="gl-s64"></div>
+						<div class="gl-s64"></div>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 </section>
