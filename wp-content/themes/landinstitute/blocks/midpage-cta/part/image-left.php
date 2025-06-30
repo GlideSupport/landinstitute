dc00d6fb2d9fb791904dc31ddcd82ae74b38ca55
Glide Design - Bansi <aakash@glidedesign.com>
2025-06-30 15:41:00 +0530

    Dev Changes

--- wp-content/themes/landinstitute/blocks/midpage-cta/part/image-left.php
+++ wp-content/themes/landinstitute/blocks/midpage-cta/part/image-left.php
@@ -5,9 +5,9 @@
 		<div class="gl-s156"></div>
 		<?php echo !empty($li_mpc_kicker)  ? '<div class="ui-eyebrow-18-16-regular sub-head">' . esc_html($li_mpc_kicker) . '</div><div class="gl-s12"></div>' : '';  ?>
 		<?php echo !empty($li_mpc_headline_check) ? BaseTheme::headline($li_mpc_headline, 'heading-2 mb-0 block-title') : ''; ?>
-		<?php echo (!empty($li_mpc_headline_check) && !empty($li_mpc_wysiwyg)) ? '<div class="gl-s30"></div>' : ''; ?>
+		<?php echo (!empty($li_mpc_headline_check) || !empty($li_mpc_wysiwyg)) ? '<div class="gl-s30"></div>' : ''; ?>
 		<?php echo !empty($li_mpc_wysiwyg)  ? '<div class="block-content body-20-18-regular">' . html_entity_decode($li_mpc_wysiwyg) . '</div>' : ''; ?>
-		<?php echo (!empty($li_mpc_wysiwyg) || !empty($li_mpc_link)) ? '<div class="gl-s52"></div>' : ''; ?>
+		<?php echo (!empty($li_mpc_wysiwyg) && !empty($li_mpc_link)) ? '<div class="gl-s52"></div>' : ''; ?>
 		<?php echo !empty($li_mpc_link) ? '<div class="block-btn">' . BaseTheme::button($li_mpc_link, 'site-btn xsm-btn') . '</div>' : ''; ?>
 		<div class="gl-s156"></div>
 	</div>
