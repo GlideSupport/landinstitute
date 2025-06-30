9541feccd67fcca2fc6b318a67e545d142af83b0
Glide Design - Bansi <aakash@glidedesign.com>
2025-06-30 20:34:46 +0530

    Dev Changes

--- wp-content/themes/landinstitute/blocks/cta-slider/block-cta-slider.php
+++ wp-content/themes/landinstitute/blocks/cta-slider/block-cta-slider.php
@@ -46,16 +46,6 @@ $li_cs_kicker = $bst_block_fields['li_cs_kicker'] ?? null;
 $li_cs_repeater = $bst_block_fields['li_cs_repeater'] ?? null;
 $li_cs_link = $bst_block_fields['li_cs_link'] ?? null;
 
-$total_cta_count = 0;
-if ($li_cs_repeater) {
-	$total_cta_count = count($li_cs_repeater);
-
-	if ($total_cta_count > 1) {
-		$swiper_class = "swiper-wrapper";
-	} else {
-		$swiper_class = "";
-	}
-}
 ?>
 
 <div class="cta-slider-block <?php echo ($total_cta_count == 1) ? 'variation-static ' : ''; ?>">
@@ -66,12 +56,11 @@ if ($li_cs_repeater) {
 		</div>
 	<?php endif; ?>
 	<?php echo (!empty($li_cs_kicker) && !empty($li_cs_headline_check)) ? '<div class="gl-s64"></div>' : ''; ?>
-	<?php if (($total_cta_count > 1)): ?>
 		<?php if (!empty($li_cs_repeater)): ?>
 			<div class="cta-slider-box">
 				<div class="swiper-container cta-work-slider">
 					<div class="slide-counter" style="display: none;">1 / 1</div>
-					<div class="<?php echo $swiper_class; ?>">
+					<div class="swiper-wrapper">
 						<?php
 						foreach ($li_cs_repeater as $li_cs_rep) :
 							$title = $li_cs_rep['title'];
@@ -106,33 +95,5 @@ if ($li_cs_repeater) {
 				</div>
 			</div>
 		<?php endif; ?>
-	<?php else : ?>
-		<?php if (!empty($li_cs_repeater)): ?>
-			<div class="cta-slider-box">
-				<div class="<?php echo $swiper_class; ?>">
-					<?php
-					foreach ($li_cs_repeater as $li_cs_rep) :
-						$title = $li_cs_rep['title'];
-						$wysiwyg = $li_cs_rep['wysiwyg'];
-						$link = $li_cs_rep['link'];
-						$image = $li_cs_rep['image'];
-						if (!empty($title) || !empty($wysiwyg) || !empty($link) || !empty($image)): ?>
-							<div class="cta-slider-lft-block">
-								<div class="cl-left">
-									<div class="slide-content">
-										<?php echo !empty($wysiwyg) ? '<div class="block-content">' . html_entity_decode($wysiwyg) . '</div><div class="gl-s64"></div>' : ''; ?>
-									</div>
-									<?php echo !empty($link) ? '<div class="block-btn">' . BaseTheme::button($link, 'site-btn text-link') . '</div><div class="gl-s64"></div>' : ''; ?>
-								</div>
-								<div class="cl-right">
-									<?php echo !empty($image) ? '<div class="cta-image">' . wp_get_attachment_image($image, 'thumb_1000') . '</div>' : ''; ?>
-								</div>
-							</div>
-						<?php endif; ?>
-					<?php endforeach; ?>
-				</div>
-			</div>
-		<?php endif; ?>
-	<?php endif; ?>
 	<?php echo !empty($li_cs_link) ? '<div class="block-ctn-btn">' . BaseTheme::button($li_cs_link, 'site-btn btn-butter-yellow') . '</div>' : ''; ?>
 </div>
