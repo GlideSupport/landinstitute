d4500e710da54bbfe997a045629805c8db2b2361
GLIDE Design - Kush <aakash@glidedesign.com>
2025-07-01 19:56:09 +0530

    Dev Changes

--- wp-content/themes/landinstitute/blocks/midpage-cta/part/text-only.php
+++ wp-content/themes/landinstitute/blocks/midpage-cta/part/text-only.php
@@ -4,7 +4,7 @@
 	<?php echo !empty($li_mpc_headline_check) ? BaseTheme::headline($li_mpc_headline, 'block-title mb-0 heading-2') : ''; ?>
 	<?php echo (!empty($li_mpc_headline_check) && !empty($li_mpc_wysiwyg)) ? '<div class="gl-s24"></div>' : ''; ?>
 	<?php echo !empty($li_mpc_wysiwyg) ? '<div class="block-content body-20-18-regular">' . html_entity_decode($li_mpc_wysiwyg) . '</div>' : ''; ?>
-	<?php echo (!empty($li_mpc_wysiwyg) && !empty($li_mpc_link)) ? '<div class="gl-s44"></div>' : ''; ?>
+	<?php echo (!empty($li_mpc_wysiwyg) || !empty($li_mpc_link)) ? '<div class="gl-s44"></div>' : ''; ?>
 	<?php echo !empty($li_mpc_link) ? '<div class="block-btn">' . BaseTheme::button($li_mpc_link, 'site-btn btn-brown xsm-btn') . '</div><div class="gl-s80"></div>' : ''; ?>
 </div>
 <?php endif; ?>
