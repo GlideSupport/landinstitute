135270b29aabe02f8824f19d74750629872b218b
GLIDE Design - Kush <aakash@glidedesign.com>
2025-07-01 20:43:13 +0530

    Dev Changes

---
+++ wp-content/themes/landinstitute/templates/template-staff.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Template Name: Staff
+ * Template Post Type: page
+ *
+ * This template is for displaying blog page.
+ *
+ * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/
+ *
+ * @package Base Theme Package
+ * @since 1.0.0
+ */
+
+// Include header.
+get_header();
+list( $bst_var_post_id, $bst_fields, $bst_option_fields ) = BaseTheme::defaults();
+?>
+
+<div id="page-section" class="page-section">
+	<!-- Content Start -->
+	<?php
+		global $wp_query;
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post();
+			// Include specific template for the content.
+			get_template_part( 'partials/content', 'page' );
+		}
+		?>
+		<?php
+	} else {
+		// If no content, include the "No posts found" template.
+		get_template_part( 'partials/content', 'none' );
+	}
+	?>
+	<!-- Content End -->
+</div>
+
+<?php
+get_footer('landing');
