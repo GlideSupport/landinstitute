c98bfeacbe140a3e34542d9b0cd587222ef76556
GLIDE Design - Kush <aakash@glidedesign.com>
2025-07-01 16:11:29 +0530

    Dev Changes

--- wp-content/themes/landinstitute/blocks/hero/part/standard.php
+++ wp-content/themes/landinstitute/blocks/hero/part/standard.php
@@ -29,7 +29,7 @@ if (!empty($li_hero_headline_check) || !empty($kicker) || !empty($wysiwyg) || !e
 						</div>
 					</div>
 					<div class="col-right">
-					<?php echo !empty($bg_image) ? '<div class="bg-pattern">' . wp_get_attachment_image($bg_image, 'thumb_800') . '</div>' : ''; ?>
+					<?php echo !empty($bg_image) ? '<div class="bg-pattern">' . wp_get_attachment_image($bg_image, 'thumb_900') . '</div>' : ''; ?>
 					<?php echo !empty($image) ? '<div class="block-image-center">' . wp_get_attachment_image($image, 'thumb_900') . '</div>' : ''; ?>
 					</div>
 				</div>
