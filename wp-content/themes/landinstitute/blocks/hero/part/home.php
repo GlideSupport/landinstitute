3b29b65e32a6c597c9d7e2b118ffb33d88c37879
Glide Design - Bansi <aakash@glidedesign.com>
2025-07-03 12:55:22 +0530

    Dev Changes

--- wp-content/themes/landinstitute/blocks/hero/part/home.php
+++ wp-content/themes/landinstitute/blocks/hero/part/home.php
@@ -27,51 +27,58 @@ if (!empty($li_hero_headline_check) || !empty($button) || !empty($image) || !emp
 							<?php echo !empty($li_hero_headline_check) ? BaseTheme::headline($li_hero_headline, 'heading-1 mb-0 block-title') : ''; ?>
 							<?php echo (!empty($li_hero_headline_check) && !empty($button)) ? '<div class="gl-s30"></div>' : ''; ?>
 							<?php echo !empty($button) ? '<div class="block-btn">' . BaseTheme::button($button, 'site-btn text-link') . '</div>' : ''; ?>
-							<div class="gl-s64"></div>
-							<div class="announcement-toggle">
-								<div class="announcement-left">
-									<div class="announcement-image">
-										<div class="video-mask"></div>
-										<img src="<?php echo esc_url($li_hero_to_image_video_thumbnail); ?>" width="129"
-											height="85" alt="video-thumb" />
-										<div class="video-play">
-											<!-- TO DO -->
-											<?php
-											if ($li_hero_to_choose_video_type === 'image') {
-												echo wp_get_attachment_image($video_thumb, 'thumb_200', false, array('class' => 'videos'));
-											} elseif ($li_hero_to_choose_video_type === 'youtube' && !empty($youtube_id)) { ?>
-												<iframe class="videos youtube" width="100%" height="100%" loading="lazy"
-													src="https://www.youtube.com/embed/<?php echo esc_attr($youtube_id); ?>?&autoplay=1&mute=1&loop=1&playlist=<?php echo esc_attr($youtube_id); ?>&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1"
-													allowfullscreen></iframe>
-												<?php
-											} elseif ($li_hero_to_choose_video_type === 'vimeo' && !empty($vimeo_id)) { ?>
-												<iframe class="videos vimeo" width="100%" height="100%" loading="lazy"
-													src="https://player.vimeo.com/video/<?php echo esc_attr($vimeo_id); ?>?&background=1&title=0&byline=0&portrait=0&sidedock=0&controls=0"></iframe>
-												<?php
-											} elseif ($li_hero_to_choose_video_type === 'upload' && !empty($video_file)) { ?>
-												<video class="videos" playsinline="playsinline" muted="muted" preload="none"
-													autoplay="autoplay" loop="loop" poster="<?php echo $video_thumb_url; ?>"
-													data-video-init="">
-													<source src="<?php echo esc_url(wp_get_attachment_url($video_file)); ?>"
-														type="video/mp4">
-												</video>
-												<?php
-											} elseif (!empty($video_thumb)) { ?>
-												<video class="videos" playsinline="playsinline" muted="muted" preload="metadata"
-													autoplay="autoplay" loop="loop" poster="<?php echo $video_thumb_url; ?>"
-													data-video-init="">
-												</video>
-											<?php } ?>
-
+							<?php if (!empty($button) && !empty($li_hero_to_image_video_thumbnail) || !empty($li_hero_to_choose_video_type) || !empty($li_hero_to_title) || !empty($li_hero_to_text)) : ?>
+								<div class="gl-s64"></div>
+							<?php endif; ?>
+							<?php if (!empty($li_hero_to_image_video_thumbnail) || !empty($li_hero_to_choose_video_type) || !empty($li_hero_to_title) || !empty($li_hero_to_text)) : ?>
+								<div class="announcement-toggle">
+								<?php if (!empty($li_hero_to_image_video_thumbnail) && !empty($li_hero_to_choose_video_type)) : ?>
+									<div class="announcement-left">
+											<div class="announcement-image">
+												<div class="video-mask"></div>
+												<img src="<?php echo esc_url($li_hero_to_image_video_thumbnail); ?>" width="129"
+													height="85" alt="video-thumb" />
+												<div class="video-play">
+													<!-- TO DO -->
+													<?php
+													if ($li_hero_to_choose_video_type === 'image') {
+														echo wp_get_attachment_image($video_thumb, 'thumb_200', false, array('class' => 'videos'));
+													} elseif ($li_hero_to_choose_video_type === 'youtube' && !empty($youtube_id)) { ?>
+														<iframe class="videos youtube" width="100%" height="100%" loading="lazy"
+															src="https://www.youtube.com/embed/<?php echo esc_attr($youtube_id); ?>?&autoplay=1&mute=1&loop=1&playlist=<?php echo esc_attr($youtube_id); ?>&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1"
+															allowfullscreen></iframe>
+														<?php
+													} elseif ($li_hero_to_choose_video_type === 'vimeo' && !empty($vimeo_id)) { ?>
+														<iframe class="videos vimeo" width="100%" height="100%" loading="lazy"
+															src="https://player.vimeo.com/video/<?php echo esc_attr($vimeo_id); ?>?&background=1&title=0&byline=0&portrait=0&sidedock=0&controls=0"></iframe>
+														<?php
+													} elseif ($li_hero_to_choose_video_type === 'upload' && !empty($video_file)) { ?>
+														<video class="videos" playsinline="playsinline" muted="muted" preload="none"
+															autoplay="autoplay" loop="loop" poster="<?php echo $video_thumb_url; ?>"
+															data-video-init="">
+															<source src="<?php echo esc_url(wp_get_attachment_url($video_file)); ?>"
+																type="video/mp4">
+														</video>
+														<?php
+													} elseif (!empty($video_thumb)) { ?>
+														<video class="videos" playsinline="playsinline" muted="muted" preload="metadata"
+															autoplay="autoplay" loop="loop" poster="<?php echo $video_thumb_url; ?>"
+															data-video-init="">
+														</video>
+													<?php } ?>
+												</div>
+											</div>
 										</div>
-									</div>
+									<?php endif; ?>
+									<?php if (!empty($li_hero_to_title) || !empty($li_hero_to_text)): ?>
+										<div class="announcement-right">
+											<?php echo !empty($li_hero_to_title) ? '<div class="ui-18-16-bold cta-title">' . esc_html($li_hero_to_title) . '</div>' : ''; ?>
+											<?php echo (!empty($li_hero_to_title) || !empty($li_hero_to_text)) ? '<div class="gl-s4"></div>' : ''; ?>
+											<?php echo !empty($li_hero_to_text) ? '<div class="ui-18-16-regular cta-content">' . esc_html($li_hero_to_text) . '</div>' : ''; ?>
+										</div>
+									<?php endif; ?>
 								</div>
-								<div class="announcement-right">
-									<?php echo !empty($li_hero_to_title) ? '<div class="ui-18-16-bold cta-title">' . esc_html($li_hero_to_title) . '</div>' : ''; ?>
-									<?php echo (!empty($li_hero_to_title) || !empty($li_hero_to_text)) ? '<div class="gl-s4"></div>' : ''; ?>
-									<?php echo !empty($li_hero_to_text) ? '<div class="ui-18-16-regular cta-content">' . esc_html($li_hero_to_text) . '</div>' : ''; ?>
-								</div>
-							</div>
+							<?php endif; ?>
 						</div>
 					</div>
 					<div class="col-right">
