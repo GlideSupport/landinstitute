ee8d2c08e75ff812c14d7925f2bdd176bcded654
Glide Design - Badal <aakash@glidedesign.com>
2025-07-04 15:26:29 +0530

    Design calendar code changes

--- wp-content/themes/landinstitute/partials/content-event-item.php
+++ wp-content/themes/landinstitute/partials/content-event-item.php
@@ -48,14 +48,14 @@ if ($current_length < $length && $last_day == 'ct-last-day-in-month') {
 	class="calendar-event <?php echo $item_class; ?> <?php echo $class; ?> <?php echo $last_day; ?> <?php echo $first_day; ?> <?php echo $has_class; ?>">
 	<?php
 	if ($current_length == 1) {
-		echo '<div class="event-title-box">';
+		echo '<div class="event-marquee">';
 		if ($thumbnail_url) {
 			echo '<img src="' . esc_url($thumbnail_url) . '" alt="" >';
 		}
 		echo '<div class="event-title-box" scrollamount="2">' . esc_html($cota_cpt_event_title) . '</div>';
 		echo '</div>';
 	} else if ($cu_day_name == "Sunday" && $current_length != 1) {
-		echo '<div class="event-title-box">';
+		echo '<div class="event-marquee">';
 		if ($thumbnail_url) {
 			echo '<img src="' . esc_url($thumbnail_url) . '" alt="" >';
 		}
