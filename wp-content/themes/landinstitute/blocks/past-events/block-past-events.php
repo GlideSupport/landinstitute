90243617e5116821c1796160ff9b4abfda41305c
Glide-Dhaval <support@glidedesign.com>
2025-07-09 11:34:45 +0530

    Update event code

--- wp-content/themes/landinstitute/blocks/past-events/block-past-events.php
+++ wp-content/themes/landinstitute/blocks/past-events/block-past-events.php
@@ -95,13 +95,18 @@ $past_events_button = $bst_block_fields['li_past_events_button'] ?? null;
 							$event_title = get_the_title($post_id);
 							$event_link = get_permalink($post_id);
 
-							$event_date = get_formatted_event_datetime($post_id)
+							$event_date = get_formatted_event_datetime($post_id);
+							$excerpt     = get_the_excerpt($post_id);
+							$event_content = wp_trim_words($excerpt, 25, '...');
+
+
+							
 							?>
 							<div class="filter-content-card-item">
 								<a href="<?php echo esc_url($event_link); ?>" class="filter-content-card-link">
 									<div class="filter-card-content">
 										<div class="gl-s52"></div>
-										<div class="eyebrow ui-eyebrow-16-15-regular"><?php echo $event_date; ?>
+										<div class="eyebrow ui-eyebrow-16-15-regular"><?php echo $event_display; ?>
 										</div>
 										<div class="gl-s6"></div>
 										<div class="card-title heading-6 mb-0"><?php echo html_entity_decode($event_title); ?></div>
