13467543281ec576953c03e8e5c2bfb7dfd649c1
Glide Design - Bansi <aakash@glidedesign.com>
2025-07-10 14:42:54 +0530

    Dev Changes

---
+++ wp-content/themes/landinstitute/partials/content-news.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template part for displaying single news
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Base Theme Package
+ * @since 1.0.0
+ */
+?>
