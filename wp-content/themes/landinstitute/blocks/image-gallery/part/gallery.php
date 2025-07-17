5d7c8b1b77f3a41ea5092d2d4c426563ccfe871b
Glide Design - Badal <aakash@glidedesign.com>
2025-07-17 15:09:23 +0530

    DD Ticket for gallery block

--- wp-content/themes/landinstitute/blocks/image-gallery/part/gallery.php
+++ wp-content/themes/landinstitute/blocks/image-gallery/part/gallery.php
@@ -7,7 +7,7 @@
 			</div>
 			<?php echo !empty($li_ig_headline_check) ? BaseTheme::headline($li_ig_headline, 'block-heading ui-128-78-bold white_text mb-0') : ''; ?>
 			<div class="cursor-static">
-				<img src="../assets/src/images/drag-button.svg" />
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/src/images/drag-button.svg" alt="Image" title="Image"/>
 			</div>
 			<?php if (!empty($li_ig_repeater)) : ?>
 				<div class="gallery-grid">
