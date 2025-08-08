d8f769b1860340a8e66abf7387a58d90d91dc389
Glide Design - Kush <aakash@glidedesign.com>
2025-08-08 17:39:26 +0530

    Dev Changes

--- wp-content/themes/landinstitute/blocks/hero/part/video.php
+++ wp-content/themes/landinstitute/blocks/hero/part/video.php
@@ -61,7 +61,7 @@ switch ($video_type) {
                     <?php if ($short_video): ?>
                         <div class="video-play-group">
                             <div class="video-play">
-                                <video class="videos" playsinline preload="metadata" autoplay loop
+                                <video class="videos" playsinline muted preload="metadata" autoplay loop
                                     poster="<?php echo esc_url($short_video_poster); ?>" data-video-init>
                                     <source src="<?php echo esc_url($short_video); ?>" type="video/mp4">
                                 </video>
