bef74f6c2b4e7958d51e0b3dcfcf151146fbeb83
Glide Design - Bansi <aakash@glidedesign.com>
2025-08-08 17:55:36 +0530

    Two Column Text changes

--- wp-content/themes/landinstitute/blocks/two-column-text/part/featured.php
+++ wp-content/themes/landinstitute/blocks/two-column-text/part/featured.php
@@ -6,30 +6,16 @@
 			$headline = $section['headline'];
 			$button = $section['link'];
 			$bg_color = $section['li_globel_bg_color_options'] ?? null;
-			if (!empty($kicker) || !empty($headline) || !empty($button) || !empty($bg_color)) {
-				$has_link = !empty($button['url']);
-				?>
+			if (!empty($kicker) || !empty($headline) || !empty($button) || !empty($bg_color)) {?>
 				<div class="text-card-col <?php echo esc_attr($bg_color); ?>">
-					<?php if ($has_link): ?>
-						<a href="<?php echo esc_url($button['url']); ?>" class="text-card-col-group"<?php echo !empty($button['target']) ? ' target="' . esc_attr($button['target']) . '"' : ''; ?>>
-					<?php else: ?>
-						<div class="text-card-col-group">
-					<?php endif; ?>
-					<div class="gl-s156"></div>
-					<?php echo !empty($kicker) ? '<div class="ui-eyebrow-20-18-regular">' . esc_html($kicker) . '</div>' : ''; ?>
-					<div class="gl-s24"></div>
-					<?php echo BaseTheme::headline($headline, 'heading-3 mb-0 block-title'); ?>
-					<div class="gl-s200"></div>
-					<?php if (!empty($button)): ?>
-						<div class="text-card-btn">
-							<div class="site-btn"><?php echo esc_html($button['title']); ?></div>
-						</div>
-					<?php endif; ?>
-					<?php if ($has_link): ?>
-						</a>
-					<?php else: ?>
-						</div>
-					<?php endif; ?>
+					<div class="text-card-col-group">
+						<div class="gl-s156"></div>
+						<?php echo !empty($kicker) ? '<div class="ui-eyebrow-20-18-regular">' . esc_html($kicker) . '</div>' : ''; ?>
+						<div class="gl-s24"></div>
+						<?php echo BaseTheme::headline($headline, 'heading-3 mb-0 block-title'); ?>
+						<div class="gl-s200"></div>
+						<?php echo !empty($button) ? '<div class="text-card-btn">' . BaseTheme::button($button, 'site-btn') . '</div>' : ''; ?>
+					</div>
 				</div>
 			<?php }
 		} ?>
