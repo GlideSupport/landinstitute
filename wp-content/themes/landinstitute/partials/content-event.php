0af30ff755788d0f74fa4ec9ef17625505e484bf
Glide Design - Kush <aakash@glidedesign.com>
2025-09-19 10:48:02 +0530

    Event form option changes

--- wp-content/themes/landinstitute/partials/content-event.php
+++ wp-content/themes/landinstitute/partials/content-event.php
@@ -214,29 +214,30 @@ $event_date = get_formatted_event_datetime($bst_var_post_id);
 		</div> <!-- wrapper -->
 	</section>
 <?php endif; ?>
-
-<?php if ($newsletter_form_visible !== false): ?>
-	<section class="container-720 bg-butter-yellow">
-		<div class="gl-s156"></div>
-		<div class="wrapper">
-			<div class="newsletter-block">
-				<div class="block-row">
-					<?php echo !empty($kicker) ? '<div class="ui-eyebrow-18-16-regular sub-head">' . esc_html($kicker) . '</div>' : ''; ?>	
-					<?php echo (!empty($kicker) && !empty($headline_check)) ? '<div class="gl-s12"></div>' : ''; ?>
-					<?php echo !empty($li_cpt_title) ? '<h2 class="heading-2 mb-0 block-title">' . esc_html($li_cpt_title) . '</h2>' : ''; ?>	
-					<?php echo (!empty($li_cpt_title) && !empty($form_selector)) ? '<div class="gl-s44"></div>' : ''; ?>
-					<div class="newsletter-form">
-						<?php if (($select_form_type == 'gravity-form')): ?>
-						<?php echo !empty($form_selector) ? do_shortcode('[gravityform id="' . $form_selector . '" title="false" ajax="true" tabindex="0"]') : ''; ?>
-						<?php else :?>
-						<?php echo !empty($form_embed) ? html_entity_decode($form_embed) : ''; ?>
-						<?php endif; ?>
+<?php if ($bst_var_select_form_type !== 'hide-form'): ?>
+	<?php if ($newsletter_form_visible !== false): ?>
+		<section class="container-720 bg-butter-yellow">
+			<div class="gl-s156"></div>
+			<div class="wrapper">
+				<div class="newsletter-block">
+					<div class="block-row">
+						<?php echo !empty($kicker) ? '<div class="ui-eyebrow-18-16-regular sub-head">' . esc_html($kicker) . '</div>' : ''; ?>	
+						<?php echo (!empty($kicker) && !empty($headline_check)) ? '<div class="gl-s12"></div>' : ''; ?>
+						<?php echo !empty($li_cpt_title) ? '<h2 class="heading-2 mb-0 block-title">' . esc_html($li_cpt_title) . '</h2>' : ''; ?>	
+						<?php echo (!empty($li_cpt_title) && !empty($form_selector)) ? '<div class="gl-s44"></div>' : ''; ?>
+						<div class="newsletter-form">
+							<?php if (($select_form_type == 'gravity-form')): ?>
+							<?php echo !empty($form_selector) ? do_shortcode('[gravityform id="' . $form_selector . '" title="false" ajax="true" tabindex="0"]') : ''; ?>
+							<?php else :?>
+							<?php echo !empty($form_embed) ? html_entity_decode($form_embed) : ''; ?>
+							<?php endif; ?>
+						</div>
+						<div class="gl-s80"></div>
 					</div>
-					<div class="gl-s80"></div>
 				</div>
 			</div>
-		</div>
-		<div class="gl-s128"></div>
-	</section>
+			<div class="gl-s128"></div>
+		</section>
+	<?php endif; ?>
 <?php endif; ?>
 <?php get_footer(); ?>
